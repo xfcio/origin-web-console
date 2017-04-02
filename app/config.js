8c0dad54f1a7f7a17674ad486a7690418018f8f7
debianmaster <9chakri+ds@gmail.com>
2017-04-01 21:37:41 -0700

    update

--- app/config.js
+++ app/config.js
@@ -6,21 +6,21 @@
 // assets/app/config.local.js and edit the copy.
 window.OPENSHIFT_CONFIG = {
   apis: {
-    hostPort: "localhost:8443",
+    hostPort: "master.ocp.ck.osecloud.com:8443",
     prefix: "/apis"
   },
   api: {
     openshift: {
-      hostPort: "localhost:8443",
+      hostPort: "master.ocp.ck.osecloud.com:8443",
       prefix: "/oapi"
     },
     k8s: {
-      hostPort: "localhost:8443",
+      hostPort: "master.ocp.ck.osecloud.com:8443",
       prefix: "/api"
     }
   },
   auth: {
-    oauth_authorize_uri: "https://localhost:8443/oauth/authorize",
+    oauth_authorize_uri: "https://master.ocp.ck.osecloud.com:8443/oauth/authorize",
     oauth_redirect_base: "https://localhost:9000/dev-console",
     oauth_client_id: "openshift-web-console",
     logout_uri: ""
